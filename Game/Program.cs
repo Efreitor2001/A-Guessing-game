aafce257f8f575500a3b68694f14b870a0612ce6
Efreitor2001 <pokimoha.123@mail.com>
2022-04-13 00:51:13 +0300

    Added hernya kakaya-ta

--- Game/Program.cs
+++ Game/Program.cs
@@ -1,20 +1,28 @@
-﻿Console.ForegroundColor = ConsoleColor.Green;
-Console.WriteLine("Компьютер загадывает число от 1 до 10...");
-Console.WriteLine("Ваша задача - отгадать это число. Удачи! ;-)");
-int num = new Random().Next(1, 10);
-Console.ForegroundColor = ConsoleColor.Red;
-Console.Write("Введите число: ");
-int num1 = Convert.ToInt32(Console.ReadLine());
-if (num == num1)
+﻿do
 {
-    Console.ForegroundColor = ConsoleColor.Blue;
-    Console.WriteLine("Вы победили! Ура-ура!!!");
-}
-else
-{
-    Console.ForegroundColor = ConsoleColor.Blue;
-    Console.WriteLine("Вы проиграли! Попробуйте ещё раз. :-(");
-    Console.Write("Загаданное число - ");
     Console.ForegroundColor = ConsoleColor.Green;
-    Console.WriteLine(num);
+    Console.WriteLine("Компьютер загадывает число от 1 до 10...");
+    Console.WriteLine("Ваша задача - отгадать это число. Удачи! ;-)");
+    Console.Write("Введите число: ");
+
+    int num1 = Convert.ToInt32(Console.Read());
+
+    int num = 2;//new Random().Next(1, 10);
+
+    Console.ForegroundColor = ConsoleColor.Red;
+    if (num1 == num)
+    {
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine("Вы победили! Ура-ура!!!");
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine("Вы проиграли! :-(");
+        Console.Write("Загаданное число - ");
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(num);
+    }
+    Console.WriteLine("Хотите попробовать ещё раз? :-) Нажмите Y, чтобы сыграть ещё раз");
 }
+while(Console.ReadKey(true).Key == ConsoleKey.Y);
