76252638ac33c45b8e0a8325b0658575797c7db8
Efreitor2001 <pokimoha.123@gmail.com>
2022-04-14 18:15:01 +0300

    Added comments

--- Game/Program.cs
+++ Game/Program.cs
@@ -1,39 +1,37 @@
-﻿do
+﻿do // Цикл
 {
-    Console.ForegroundColor = ConsoleColor.Green;
-    Console.WriteLine("Компьютер загадывает число от 1 до 10...");
-    Console.WriteLine("Ваша задача - отгадать это число. Удачи! ;-)");
-    int num = new Random().Next(1, 10);
-    Console.ForegroundColor = ConsoleColor.Red;
-    Console.Write("Введите число: ");
-    var num1 = Console.ReadLine();
-    int Num1;
-    bool IsNum = int.TryParse(num1, out Num1);
-    while (IsNum == false)
-        if (IsNum == false)
-        {
-            Console.Write("Это не число, попробуй ещё раз: ");
-            num1 = Console.ReadLine();
-            IsNum = int.TryParse(num1, out Num1);
-        }
-    while (Num1 > 10 | Num1 < 1)
+    Console.ForegroundColor = ConsoleColor.Green; // Цвет текста = зелёный
+    Console.WriteLine("Компьютер загадывает число от 1 до 10..."); //Вывод
+    Console.WriteLine("Ваша задача - отгадать это число. Удачи! ;-)"); // Вывод
+    int num = new Random().Next(1, 10); // Рандомит число от 1 до 10 и записывает в num
+    Console.ForegroundColor = ConsoleColor.Red; // Цвет текста = красный
+    Console.Write("Введите число: "); // Вывод
+    var num1 = Console.ReadLine(); // Ввод числа от пользователя
+    int Num1; // Целочисленная переменная
+    bool IsNum = int.TryParse(num1, out Num1); // IsNum = true or false, если num1 - это число, то значение = true
+    while (IsNum == false) // Цикл, пока IsNum имеет значение false
     {
-        if (Num1 > 10)
+        Console.Write("Это не число, попробуй ещё раз: "); // Вывод
+        num1 = Console.ReadLine(); // Ввод число от пользователся
+        IsNum = int.TryParse(num1, out Num1); // IsNum = true or false, если num1 - это число, то значение = true
+    }
+    while (Num1 > 10 | Num1 < 1) // Цикл, пока Num1 > 10 или < 1
+    {
+        if (Num1 > 10) // Если больше 10
         {
-            Console.WriteLine("Смысл вводить число больше 10? xD");
-            Console.Write("Не дури, введи число ещё раз: ");
-            var num2 = Console.ReadLine();
-            int Num2;
-            bool IsNum2 = int.TryParse(num2, out Num2);
-            Num1 = Num2;
-            while (IsNum2 == false)
-                if (IsNum2 == false)
-                {
-                    Console.Write("Это не число, попробуй ещё раз: ");
-                    num2 = Console.ReadLine();
-                    IsNum2 = int.TryParse(num2, out Num2);
-                    Num1 = Num2;
-                }
+            Console.WriteLine("Смысл вводить число больше 10? xD"); // Вывод
+            Console.Write("Не дури, введи число ещё раз: "); // Вывод
+            var num2 = Console.ReadLine(); // Ввод число от пользователя
+            int Num2; // Целочисленная переменная
+            bool IsNum2 = int.TryParse(num2, out Num2); // IsNum2 = true or false, если num1 - это число, то значение = true
+            Num1 = Num2; // Значение из Num2 перетекает в Num1
+            while (IsNum2 == false) // Цикл, пока IsNum имеет значение false
+            {
+                Console.Write("Это не число, попробуй ещё раз: "); // Вывод
+                num2 = Console.ReadLine(); // Ввод число от пользователя
+                IsNum2 = int.TryParse(num2, out Num2); // IsNum2 = true or false, если num1 - это число, то значение = true
+                Num1 = Num2; // Значение из Num2 перетекает в Num1
+            }
         }
         if (Num1 < 1)
         {
@@ -53,7 +51,7 @@
                 }
         }
     }
-    if (num == Num1)
+    if (num == Num1) // Условия к победе
     {
         Console.ForegroundColor = ConsoleColor.Blue;
         Console.WriteLine("Вы победили! Ура-ура!!!");
@@ -61,7 +59,7 @@
         Console.ForegroundColor = ConsoleColor.Green;
         Console.WriteLine(num);
     }
-    else
+    else // Лошара, проиграл
     {
         Console.ForegroundColor = ConsoleColor.Blue;
         Console.WriteLine("Вы проиграли! Попробуйте ещё раз. :-(");
@@ -71,4 +69,4 @@
     }
     Console.WriteLine("Нажмите Y, чтобы сыграть ещё раз! :-)");
 }
-while (Console.ReadKey(true).Key == ConsoleKey.Y);
+while (Console.ReadKey(true).Key == ConsoleKey.Y); // Условие цикла, Если пользователь нажмёт кнопку Y, то идём на повтор
